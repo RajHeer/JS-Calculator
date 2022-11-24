8901f5faa441a372100564bb6b8231d5ee8b5d1d
Raj Heer <thisisrajmatazz@gmail.com>
2022-11-24 11:48:53 +0000

    Setup html, js files, which has basic operation functions.

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log("Connected to JS file");
+
+// BASIC OPERATIONS ***
+
+function add (num1, num2) {
+    return num1 + num2;
+}
+
+function subtract (num1, num2) {
+    return num1 - num2;
+}
+
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+
+function divide (num1, num2) {
+    return num1 / num2;
+}
+
+console.log("Add " + add(1,2));
+console.log("Subtract " + subtract(1,2));
+console.log("Multiply " + multiply(1,2));
+console.log("Divide " + divide(1,2));
