6f3faa4750ecb32e15e81de8e10b5bc79bce5138
Raj Heer <thisisrajmatazz@gmail.com>
2022-11-26 14:33:52 +0000

    Add operate function to link all basic functions

--- main.js
+++ main.js
@@ -18,7 +18,27 @@ function divide (num1, num2) {
     return num1 / num2;
 }
 
+//OPERATOR FUNCTION
+
+function operate (operator, num1, num2) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+            break;
+        case "-":
+            return subtract(num1, num2);
+            break;
+        case "*":
+            return multiply(num1, num2);
+            break;
+        case "/":
+            return divide(num1, num2);
+            break;
+    }
+}
+
 console.log("Add " + add(1,2));
 console.log("Subtract " + subtract(1,2));
 console.log("Multiply " + multiply(1,2));
 console.log("Divide " + divide(1,2));
+console.log("Operator " + operate("/",1,2));
